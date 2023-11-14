7abef2ffd9fc70db7d220650b057f6973b88cbb4
dferris111 <daltonferris111@gmail.com>
2023-11-14 11:41:37 -0500

    added dummy_enemy to test hitbox
    no test done with hitbox yet, just setting up.

---
+++ Entities/Enemies/dummy_enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var health = 3
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if health <= 0:
+		queue_free()
+
+	move_and_slide()
