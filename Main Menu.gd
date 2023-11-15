9822362a43c0571b6b50f95c163dd59e82ed6551
MidnightIce <aaliyahlynn1024@gmail.com>
2023-11-14 19:45:20 -0500

    Main Menu Changes
    Changed the main menu to add multiplayer and linked it to the multiplayer screen. Added a background too.

--- Main Menu.gd
+++ Main Menu.gd
@@ -22,3 +22,7 @@ func _on_options_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_multiplayer_pressed():
+	get_tree().change_scene_to_file("res://Networking/PeerToPeerConnection.tscn")
