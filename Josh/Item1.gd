a6abe586365af79c71d7cf1aa0ea0932b0e642fd
JoshCain <54875029+Josh-svg@users.noreply.github.com>
2023-11-27 17:14:23 -0500

    Adding Resources and scenes

---
+++ Josh/Item1.gd
@@ -0,0 +1,11 @@
+class_name ItemResources extends Resource
+
+@export var itemName: String = "Sword"
+@export var category: String = "Weapon"
+@export var stackSize: int = 1
+@export var damage: int = 5
+@export var multipleTargets: bool = false
+@export var radius: int = 2
+@export var stunTime: int = 0
+@export var timeDelay: float = 3.0
+@export var textures: Texture = preload("res://Josh/Art/Sword.png")
