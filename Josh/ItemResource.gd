3bfb8e46f95ec8facba0dc7dedf5bede0179cfa6
JoshCain <54875029+Josh-svg@users.noreply.github.com>
2023-12-01 23:56:53 -0500

    Fixing code and textures on .tres

--- Josh/ItemResource.gd
+++ Josh/ItemResource.gd
@@ -2,12 +2,8 @@ class_name ItemResources extends Resource
 
 @export var itemName: String = "Sword"
 @export var category: String = "Weapon"
+@export var isStaff: bool = false
 @export var stackSize: int = 1
-@export var damage: int = 5
-@export var multipleTargets: bool = false
-@export var radius: int = 2
-@export var stunTime: int = 0
 @export var timeDelay: float = 3.0
 
 @export var gui_texture : Texture = preload("res://Josh/Art/Sword.png")
-@export var use_texture : Texture = preload("res://Josh/Art/Sword.png")
